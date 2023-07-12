2ba4d301497bf2dd7176ff1ab5b09daf9de72047
krishpranav <krisna.pranav@gmail.com>
2023-07-12 20:30:31 +0530

    componenet: version

--- component/version.h
+++ component/version.h
@@ -9,26 +9,37 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
 #include <stdbool.h>
 #include "types.h"
 
-#define NANO_VERSION_MAJOR 1
-#define NANO_VERSION_MINOR 0
-#define NANO_VERSION_PATCH 0
+#define NANO_VERSION_MAJOR               1
+#define NANO_VERSION_MINOR               0
+#define NANO_VERSION_PATCH               0
 
-#define NANO_VERSION_STRING "1.0.0"
+#define NANO_VERSION_STRING         "1.0.0"
+#define NANO_VERSION_NUMBER         ((NANO_VERSION_MAJOR << 24) | \
+                                     (NANO_VERSION_MINOR << 16) | \
+                                     (NANO_VERSION_PATCH <<  8))
 
-
-#ifndef __cplusplus
-extern "C"
-{
-    NUINT32 NanoIOTSdkVersion(void);
-
-    NCHAR *NanoIOTSdkVersionString(void);
-#ifndef __cplusplus
-}
+#ifdef __cplusplus
+extern "C" {
 #endif
 
+    /**
+     * @return NUINT32 
+     */
+    NUINT32 NanoIOTSdkVersion(void);
+
+    /**
+     * @return NCHAR* 
+     */
+    NCHAR *NanoIOTSdkVersionString(void);
+
+
+#ifdef __cplusplus
+}
 #endif 
+
+#endif
